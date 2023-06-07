d1387ad79939d8de55a891c1a035e8e093509a4d
Denis Yanbakhtov <denis.the.student@gmail.com>
2023-06-07 13:37:26 +0300

    Complete homework1

---
+++ src/com/example/lesson1/Homework1.java
@@ -0,0 +1,10 @@
+package com.example.lesson1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1() {
+        System.out.println("solution 1");
+    }
+}
